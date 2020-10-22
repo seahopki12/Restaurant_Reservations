10236a41334de1a7ae3d1b5318c3836b44365f17
Sean Hopkins <seahopki12@gmail.com>
2020-10-21 21:14:46 -0400

    finished get and post routes for backend.

--- server.js
+++ server.js
@@ -11,19 +11,36 @@ const waitList = [];
 
 const reservations = [];
 
-app.get("/", function(req, res) {
+app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
+})
+
+app.get("/tables", function (req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+})
+
+app.get("/reserve", function (req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+})
+
+app.get("/api/tables", function (req, res) {
+    res.json(reservations);
 });
 
-app.get("/tables", function(req, res) {
-  res.json(reservations);
-  res.json(waitList);
+app.get("/api/waitlist", function (req, res) {
+    res.json(waitList);
 });
 
-app.get("/reserve", function(req, res) {
-  console.log("This is where the reservation form will go.")
+app.post("/api/tables", function (req, res) {
+    if (reservations.length < 5) {
+        reservations.push(req.body);
+        res.end(true);
+    } else {
+        waitList.push(req.body);
+        res.end(false);
+    };
 });
 
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
 });
