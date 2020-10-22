ce1e890de9616be81f648215575aa54ea6e05725
Sean Hopkins <seahopki12@gmail.com>
2020-10-21 20:24:09 -0400

    get_homepage

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
-<<<<<<< HEAD
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const waitList = [];
 
 const reservations = [];
 
 app.get("/", function(req, res) {
-  res.send("This is where the home page will go!");
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/tables", function(req, res) {
   res.json(reservations);
   res.json(waitList);
-  console.log("This is where the lists will go.")
 });
 
 app.get("/reserve", function(req, res) {
@@ -25,18 +27,3 @@ app.get("/reserve", function(req, res) {
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
-=======
-// Dependencies
-
-// Sets up the Express App
-
-// Sets up the Express app to handle data parsing
-
-// DATA
-
-// Routes
-
-// Create new table reservations
-
-// Starts the server to begin listening
->>>>>>> 66e5e766d80501a15a336594e2254fbf77c2b8d0
